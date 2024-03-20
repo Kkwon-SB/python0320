4f6224a188a01e1c4716c7e1a58e59ba4f3df656
권순빈/테이블에이아이/q93mr2 <q93mr2@ssg.global>
2024-03-20 18:45:23 +0900

    add all

--- work.py
+++ work.py
@@ -1,9 +1,12 @@
 
 import json
 import pandas as pd
+import csv
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
+
+
 
 result_columns = [
     'sale_date',
@@ -22,26 +25,11 @@ result_columns = [
 
 order_df = pd.read_csv('test.csv')
 
-
-result_df2 = order_df.sort_values(by=['sale_date','store_code','pos_no','seq_no'], ascending=False)
-
-cnt = 0
-
-for index, data in result_df2.iterrows():
-    # print(f"{data['sale_date']}|{data['store_code']}|{data['pos_no']}|{data['seq_no']}") 
-    print(index, data)
-
-    cnt += 1
-    if cnt > 20:
-        break
-
-
-order_df = order_df.drop(['st_code'], axis=1)
-
-order_df['sku_qnty'] = order_df['sku_qnty'].fillna(0) 
+order_df['sku_qnty'] = order_df['sku_qnty'].fillna(0)
 order_df['sku_stlmn_amnt'] = order_df['sku_stlmn_amnt'].fillna(0)
-order_df['sku_no'] = order_df['sku_no'].fillna('') 
-order_df = order_df.astype({ 
+order_df['sku_no'] = order_df['sku_no'].fillna('')
+
+order_df = order_df.astype({
         'total_qnty': 'int',
         'total_sell_amnt': 'int',
         'total_dscn_amnt': 'int',
@@ -50,141 +38,122 @@ order_df = order_df.astype({
         'sku_qnty': 'int'
     })
 
-
-cnt = 0
-
-
-order_group_df = order_df.groupby(['sale_date', 'store_code', 'pos_no', 'seq_no'], as_index=False) 
+order_df.info()
 
 
-def to_json_custom_total(group): 
-    data = group.to_dict('records')
-    result_data = []
+# result_df2 = order_df.sort_values(by=['sale_date','store_code','pos_no','seq_no'], ascending=False)
 
-    for datum in data:
-        result_datum = {
-            'total_qnty': datum.get('total_qnty'),
-            'total_sell_amnt': datum.get('total_sell_amnt'),
-            'total_dscn_amnt': datum.get('total_dscn_amnt'),
-            'total_stlmn_amnt': datum.get('total_stlmn_amnt')
-        }
-        result_data.append(result_datum)
+# array = list(order_df.columns.values)
 
-    return json.dumps(list(result_data))
 
-def to_json_custom_sku(group): #sku 개수
-    data = group.to_dict('records')
-    result_data = []
+# def load_csv_to_array(file_path):
+#     data_array = []
+#     with open(file_path, 'r', newline='') as csvfile:
+#         csvreader = csv.reader(csvfile)
+#         for row in csvreader:
+#             data_array.append(row)
+#     return data_array
 
-    for datum in data:
-        result_datum = {
-            'sku_no': datum.get('sku_no'),
-            'sku_stlmn_amnt': datum.get('sku_stlmn_amnt'),
-            'sku_qnty': datum.get('sku_qnty')
-        }
-        result_data.append(result_datum)
 
-    return json.dumps(list(result_data))
+# csv_data = load_csv_to_array('test2.csv')
 
-def to_json_custom_cpn(group): #쿠폰 개수
-    data = group.to_dict('records')
-    result_data = []
+# # 배열 출력
+# # for row in csv_data:
+# #     print(row)
 
-    for datum in data:
-        result_data.append(datum.get('cpn_no'))
+# # print(len(csv_data))
 
-    return json.dumps(list(result_data))
 
-def to_json_custom_total_qnty(group): #쿠폰
-    data = group.to_dict('records')
-    result_data = []
-
-    for datum in data:
-        result_data.append(datum.get('total_sell_amnt'))
-
-    return json.dumps(list(result_data))
-
-def to_json_custom_total_sell_amnt(group): #쿠폰
-    data = group.to_dict('records')
-    result_data = []
-
-    for datum in data:
-        result_data.append(datum.get('total_dscn_amnt'))
-
-    return json.dumps(list(result_data))
-
-def to_json_custom_total_dscn_amnt(group): #쿠폰
-    data = group.to_dict('records')
-    result_data = []
-
-    for datum in data:
-        result_data.append(datum.get('total_stlmn_amnt'))
-
-    return json.dumps(list(result_data))
-
-def to_json_custom_total_stlmn_amnt(group): #쿠폰
-    data = group.to_dict('records')
-    result_data = []
-
-    for datum in data:
-        result_data.append(datum.get('total_qnty'))
-
-    return json.dumps(list(result_data))
-
-def to_json_custom_store(group): #가게 개수
-    data = group.to_dict('records')
-    result_data = []
-
-    for datum in data:
-        result_data.append(datum.get('store_code'))
-
-    return json.dumps(list(result_data))
-
-def to_json_custom_pos(group): #pos 개수
-    data = group.to_dict('records')
-    result_data = []
-
-    for datum in data:
-        result_data.append(datum.get('pos_no'))
-
-    return json.dumps(list(result_data))
-
-def to_json_custom_seq(group): #seq 개수
-    data = group.to_dict('records')
-    result_data = []
-
-    for datum in data:
-        result_data.append(datum.get('seq_no'))
-
-    return json.dumps(list(result_data))
-
-def to_json_custom_seq(group): #seq 개수
-    data = group.to_dict('records')
-    result_data = []
-
-    for datum in data:
-        result_data.append(datum.get('seq_no'))
-
-    return json.dumps(list(result_data))
+# for i in csv_data:
+#     if i[0] == '20240318' and i[1] == "4089" and i[2] == "03" and i[3] == "0025":
+#         print(i) 
 
 
 
-order_df['sku_cntnt'] = order_group_df.apply(to_json_custom_sku).drop(columns=['sale_date', 'store_code', 'pos_no', 'seq_no'])
-order_df['cpn_cntnt'] = order_group_df.apply(to_json_custom_cpn).drop(columns=['sale_date', 'store_code', 'pos_no', 'seq_no'])
-order_df['order_dtm'] = order_df['sale_date'] + order_df['trade_time']
-
-result_df = order_df[result_columns]
-
-result_df2 = result_df.sort_values(by=['sale_date','store_code','pos_no','seq_no'], ascending=False)
 
 
-# result_df2.info()
+
+
+
+
+
+
+
+
+
 
 # cnt = 0
 
 # for index, data in result_df2.iterrows():
-#     print(f"{data['sale_date']}|{data['store_code']}|{data['pos_no']}|{data['seq_no']}|{data['sku_cntnt']}") 
+#     # print(f"{data['sale_date']}|{data['store_code']}|{data['pos_no']}|{data['seq_no']}") 
+#     print("ㅡㅡㅡㅡㅡ\n",index, data)
 
 #     cnt += 1
-#     if cnt > 20:
+#     if cnt > 100:
 #         break
+
+
+# order_df = order_df.drop(['st_code'], axis=1)
+
+# order_df['sku_qnty'] = order_df['sku_qnty'].fillna(0) 
+# order_df['sku_stlmn_amnt'] = order_df['sku_stlmn_amnt'].fillna(0)
+# order_df['sku_no'] = order_df['sku_no'].fillna('') 
+# order_df = order_df.astype({ 
+#         'total_qnty': 'int',
+#         'total_sell_amnt': 'int',
+#         'total_dscn_amnt': 'int',
+#         'total_stlmn_amnt': 'int',
+#         'sku_stlmn_amnt': 'int',
+#         'sku_qnty': 'int'
+#     })
+
+
+# cnt = 0
+
+
+# order_group_df = order_df.groupby(['sale_date', 'store_code', 'pos_no', 'seq_no'], as_index=False) 
+
+
+# def to_json_custom_sku(group): #sku 개수
+#     data = group.to_dict('records')
+#     result_data = []
+
+#     for datum in data:
+#         result_datum = {
+#             'sku_no': datum.get('sku_no'),
+#             'sku_stlmn_amnt': datum.get('sku_stlmn_amnt'),
+#             'sku_qnty': datum.get('sku_qnty')
+#         }
+#         result_data.append(result_datum)
+
+#     return json.dumps(list(result_data))
+
+# def to_json_custom_cpn(group): #쿠폰 개수
+#     data = group.to_dict('records')
+#     result_data = []
+
+#     for datum in data:
+#         result_data.append(datum.get('cpn_no'))
+
+#     return json.dumps(list(result_data))
+
+
+# order_df['sku_cntnt'] = order_group_df.apply(to_json_custom_sku).drop(columns=['sale_date', 'store_code', 'pos_no', 'seq_no'])
+# order_df['cpn_cntnt'] = order_group_df.apply(to_json_custom_cpn).drop(columns=['sale_date', 'store_code', 'pos_no', 'seq_no'])
+# order_df['order_dtm'] = order_df['sale_date'] + order_df['trade_time']
+
+# result_df = order_df[result_columns]
+
+# # result_df2 = result_df.sort_values(by=['sale_date','store_code','pos_no','seq_no'], ascending=False)
+
+
+# # result_df2.info()
+
+# # cnt = 0
+
+# # for index, data in result_df2.iterrows():
+# #     print(f"{data['sale_date']}|{data['store_code']}|{data['pos_no']}|{data['seq_no']}|{data['sku_cntnt']}") 
+
+# #     cnt += 1
+# #     if cnt > 20:
+# #         break
